ede1af60e6a326e5ab1af7b5034997b56566aa04
hixb <Hello16369@163.com>
2022-07-17 11:13:37 +0800

    refactor: 重构头部

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -1,300 +1,70 @@
 <template>
   <header>
-    <div ref="scheduleBar" class="schedule"></div>
-    <nuxt-link to="/">
-      <h1>
-        <img src="~/assets/images/logo.png" alt="logo">
-        <span>{{ $store.state.APP_PROJECT_NAME }}</span>
-      </h1>
-    </nuxt-link>
-    <div class="links">
-      <div class="color-picker">
-        <i class="iconfont ice-color" @click="isShowPicker = !isShowPicker" />
-        <div :class="['color-picker-menu', !isShowPicker ? 'hide-picker' : 'show-picker']">
-          <span class="title">选择颜色</span>
-          <ul>
-            <li v-for="(item, index) in colorPickerSelect" :key="item.id" :class="{active: defaultTheme === index}"
-                @click="toggleTheme(index)"
-            >
-              {{ item.name }}
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="search">
-        <i class="iconfont ice-search" />
-        <input
-          v-model="keyword" v-focus="focusState" type="text" :class="{focused: focusState}"
-          @click="focusState = true" @blur="focusState = false" @keypress="enterSearch"
+    <div class="btn-area">
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+    <div class="menu-links">
+      <div class="theme-toggle" @click="themeToggle">
+        <svg
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="1.5"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         >
+          <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
+        </svg>
       </div>
-      <div class="nav-links">
-        <div v-for="(item, index) in navLink" :key="index"
-             :class="['nav-item', item.children.length > 0 ? 'drop-down' : '']"
-        >
-          <nuxt-link :to="item.link" class="link">
-            <i :class="['iconfont', item.icon]" />
-            <span>{{ item.title }}</span>
-            <i v-if="item.children.length > 0" class="arrow" />
+      <div class="search-bar">
+        <input type="text">
+      </div>
+      <ul class="menu">
+        <li>
+          <nuxt-link to="/">
+            Apps
           </nuxt-link>
-          <ul v-if="item.children.length > 0">
-            <li v-for="(nav, i) in item.children" :key="i">
-              <nuxt-link :to="nav.link">
-                <i :class="['iconfont', nav.icon]" />
-                <span>{{ nav.name }}</span>
-              </nuxt-link>
-            </li>
-          </ul>
+        </li>
+        <li>
+          <nuxt-link to="/">
+            Yourwork
+          </nuxt-link>
+        </li>
+      </ul>
+      <div class="profile">
+        <div class="notification">
+          <span>3</span>
+          <svg
+            viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+            stroke-linejoin="round" class="feather feather-bell"
+          >
+            <path d="M18 8A6 6 0 006 8c0 7-3 9-3 9h18s-3-2-3-9M13.73 21a2 2 0 01-3.46 0" />
+          </svg>
         </div>
-        <vs-button v-if="!isLogin" success flat class="login-btn" :active="active === 1" @click="showLoginModel">
+        <vs-button success flat class="login-btn">
           <i class="iconfont ice-lock"></i>
           登录
         </vs-button>
-        <div v-else class="avatar">
-          <vs-avatar badge badge-color="success" class="user-avatar">
-            <img src="https://vuesax.com/avatars/avatar-6.png" alt="">
-          </vs-avatar>
-          <ul class="info-list">
-            <li @click="$router.push('/member')">
-              个人中心
-            </li>
-            <li @click="outLogin">
-              退出登录
-            </li>
-          </ul>
-        </div>
       </div>
     </div>
-    <ExpandSearch />
   </header>
 </template>
 
-<script>
-import { getLocalStorage, setLocalStorage } from "@/plugins/utils";
-import ExpandSearch from "@/components/common/ExpandSearch";
-
-const dark = [ "rgba(24, 24, 24)", "rgba(255, 255, 255, 0.8)" ];
-const light = [ "rgba(255, 255, 255)", "rgba(44, 62, 80)" ];
-const otherDark = [ "rgba(17, 17, 17)" ];
-const otherLight = [ "rgba(243, 244, 244)" ];
-const darkShadow = [ "0 1px 8px 0 rgba(0, 0, 0, 0.6)", "0 2px 16px 0 rgba(0, 0, 0, 0.7)" ];
-const lightShadow = [ "0 1px 8px 0 rgba(0, 0, 0, 0.2)", "0 2px 16px 0 rgba(0, 0, 0, 0.2)" ];
-const darkBorColor = [ "rgba(0, 0, 0, 0.3)" ];
-const lightBorColor = [ "rgba(234, 236, 239)" ];
-
-export default {
+<script>export default {
   name: "Header",
-  components: { ExpandSearch },
-  directives: {
-    focus: {
-      update: function(el, { value }) {
-        if (value) {
-          el.focus();
-        }
-      }
-    }
-  },
   data() {
     return {
-      keyword: "",
-      oldKeyword: "",
-      active: -1,
-      isLogin: false,
-      colorPickerSelect: [
-        {
-          id: 0,
-          name: "dark"
-        },
-        {
-          id: 1,
-          name: "auto"
-        },
-        {
-          id: 2,
-          name: "light"
-        },
-      ],
-      defaultTheme: 1,
-      isShowPicker: false,
-      focusState: false,
-      navLink: [
-        {
-          title: "首页",
-          icon: "ice-home",
-          link: "",
-          children: []
-        },
-        {
-          title: "分类",
-          icon: "ice-category",
-          link: "",
-          children: [
-            {
-              name: "vue",
-              link: "",
-              icon: ""
-            },
-            {
-              name: "js",
-              link: "",
-              icon: ""
-            }
-          ]
-        },
-        {
-          title: "标签",
-          icon: "ice-tag",
-          link: "",
-          children: []
-        },
-        {
-          title: "时间线",
-          icon: "ice-date",
-          link: "",
-          children: []
-        },
-        {
-          title: "联系",
-          icon: "ice-message",
-          link: "",
-          children: [
-            {
-              name: "GITHUB",
-              link: "",
-              icon: ""
-            },
-            {
-              name: "GITEE",
-              link: "",
-              icon: ""
-            }
-          ]
-        }
-      ]
+      isBrightTheme: false
     };
-  },
-  watch: {
-    keyword(newVal) {
-      if (newVal === "" && this.oldKeyword !== "") {
-        this.oldKeyword = "";
-      }
-    }
-  },
-  mounted() {
-    /** 判断是否登录 */
-    this.isLogin = this.$utils.isLogin();
-
-    /** 点击任意元素关闭颜色选择器 */
-    document.addEventListener("click", e => {
-      if (!this.$el.contains(e.target)) {
-        this.isShowPicker = false;
-      }
-    });
-
-    /** 添加顶部滚动进度条 */
-    const pageHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
-    const windowHeight = document.documentElement.clientHeight || document.body.clientHeight;
-    const scrollAvail = pageHeight - windowHeight;
-    window.onscroll = () => {
-      let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-      this.$refs.scheduleBar.style.width = (scrollTop / scrollAvail) * 100 + "%";
-    };
-
-    /** 设置系统主题 */
-    const localTheme = JSON.parse(getLocalStorage("my_theme"));
-    if (localTheme && localTheme.length > 0) {
-      this.defaultTheme = localTheme.find(item => item.id + 1).id;
-      this.currentTheme(localTheme[0], localTheme[1], localTheme[2], localTheme[3], localTheme[4], localTheme[5]);
-    } else {
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? this.currentTheme(dark[0], dark[1], otherDark[0], darkShadow[0], darkShadow[1], darkBorColor[0])
-        : this.currentTheme(light[0], light[1], otherLight[0], lightShadow[0], lightShadow[1], lightBorColor[0]);
-    }
   },
   methods: {
-    /** 切换主题 */
-    toggleTheme(index) {
-      this.defaultTheme = index;
-      const curTheme = this.themeIf(index);
-      switch (index) {
-        case 0:
-        case 2:
-          this.currentTheme(curTheme[0], curTheme[1], curTheme[2], curTheme[3], curTheme[4], curTheme[5]);
-          break;
-        case 1:
-          window.matchMedia("(prefers-color-scheme: dark)").matches
-            ? this.currentTheme(curTheme[0], curTheme[1], curTheme[2], curTheme[3], curTheme[4], curTheme[5])
-            : this.currentTheme(curTheme[0], curTheme[1], curTheme[2], curTheme[3], curTheme[4], curTheme[5]);
-          break;
-      }
-      curTheme.push({ id: index });
-      setLocalStorage("my_theme", JSON.stringify(curTheme));
-    },
-    /** 主题判断换 */
-    themeIf(index) {
-      let themeBgColor, themeTextColor, themeOterBgc, curShadow, curShadowHov, curBorColor;
-      switch (index) {
-        case 0:
-          themeBgColor = dark[0];
-          themeTextColor = dark[1];
-          themeOterBgc = otherDark[0];
-          curShadow = darkShadow[0];
-          curShadowHov = darkShadow[1];
-          curBorColor = darkBorColor[0];
-          break;
-        case 2:
-          themeBgColor = light[0];
-          themeTextColor = light[1];
-          themeOterBgc = otherLight[0];
-          curShadow = lightShadow[0];
-          curShadowHov = lightShadow[1];
-          curBorColor = lightBorColor[0];
-          break;
-        default:
-          themeBgColor = dark[0];
-          themeTextColor = dark[1];
-          themeOterBgc = otherDark[0];
-          curShadow = darkShadow[0];
-          curShadowHov = darkShadow[1];
-          curBorColor = darkBorColor[0];
-          break;
-      }
-      return [ themeBgColor, themeTextColor, themeOterBgc, curShadow, curShadowHov, curBorColor ];
-    },
-    /** 设置当前主题为 */
-    currentTheme(curThemeBgc, curThemeColor, curOterBgc, curShadow, curShadowHov, curBorColor) {
-      document.documentElement.style.setProperty("--my-cur-default-theme-bgc", curThemeBgc);
-      document.documentElement.style.setProperty("--my-cur-default-theme-color", curThemeColor);
-      document.documentElement.style.setProperty("--my-cur-default-theme-oter", curOterBgc);
-      document.documentElement.style.setProperty("--my-cur-default-theme-shadow", curShadow);
-      document.documentElement.style.setProperty("--my-cur-default-theme-shadow-hover", curShadowHov);
-      document.documentElement.style.setProperty("--my-cur-default-theme-bor-color", curBorColor);
-    },
-    /** 搜索 */
-    enterSearch(e) {
-      if (e.keyCode === 13) {
-        if (this.keyword === "" && this.oldKeyword === "") {
-          this.$utils.toast("warn", "输入内容为空");
-          return;
-        }
-        if (this.keyword !== "") {
-          this.oldKeyword = this.keyword;
-        }
-        console.log(this.keyword);
-      }
-    },
-    showLoginModel() {
-      this.active = 1;
-      this.$loginModel.show();
-    },
-    outLogin() {
-      this.$cookies.remove("is_login");
-      if (!this.$cookies.get("is_login")) {
-        this.$utils.toast("success", "登出成功");
-        setTimeout(() => {
-          location.reload();
-        }, 2000);
-      }
+    themeToggle() {
+      this.isBrightTheme = !this.isBrightTheme;
+      this.$emit("themeToggle", this.isBrightTheme);
     }
   }
 };
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,73 @@
 <template>
-  <div>
-    <Header />
-    <main class="main">
-      <Nuxt />
-      <GoBack />
-    </main>
-    <Footer />
+  <div :class="['app', currentTheme]">
+    <div class="app-box">
+      <Header @themeToggle="themeToggle" />
+      <main class="main">
+        <Nuxt />
+        <!--      <GoBack />-->
+      </main>
+      <!--    <Footer />-->
+    </div>
+    <div class="video-bg">
+      <video autoplay loop muted>
+        <source src="https://assets.codepen.io/3364143/7btrrd.mp4">
+      </video>
+    </div>
   </div>
 </template>
 
 <script>
 import Header from "@/components/common/Header";
-import Footer from "@/components/common/Footer";
-import GoBack from "@/components/common/GoBack";
+// import Footer from "@/components/common/Footer";
+// import GoBack from "@/components/common/GoBack";
 
 export default {
   components: {
-    GoBack,
-    Header,
-    Footer
+    Header
+  },
+  data() {
+    return {
+      currentTheme: ""
+    };
+  },
+  methods: {
+    themeToggle(val) {
+      this.currentTheme = val ? "light-mode" : "";
+      console.log(val);
+    }
   }
 };
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+
+.app {
+  .app-box {
+    .bgc(var(--my-theme-bg-color));
+    .area(90%, 90vh);
+    border-radius: 14px;
+    backdrop-filter: blur(20px);
+    .pof;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 2;
+
+    .main {
+      padding: 0 30px;
+    }
+  }
+}
+
+.video-bg {
+  .pof;
+  right: 0;
+  top: 0;
+  .area(100%, 100%);
+
+  video {
+    .area(100%, 100%);
+    object-fit: cover;
+  }
+}
+</style>
