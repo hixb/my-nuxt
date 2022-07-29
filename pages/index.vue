d541e932060f9f6a583647ff9d71ef21ddb58abf
hixb <Hello16369@163.com>
2022-07-29 21:10:33 +0800

    chore: integrated sass

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>hello</div>
+  <div class="home">
+    <span>hello</span>
+  </div>
 </template>
 
-<style scoped lang=""></style>
+<style scoped lang="scss">
+.home {
+  > span {
+    color: red;
+  }
+}
+</style>
