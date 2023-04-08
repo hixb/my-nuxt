ced660ed0a38ad9a84d7800bdd3bb336616b210f
hixb <Hello16369@163.com>
2023-04-08 22:39:01 +0800

    chore(rely): add `@nuxt/image-edge` dependency package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
     },
     plugins: [stylelintPlugin()]
   },
-  modules: ["@pinia/nuxt", "@nuxtjs/i18n", "@vueuse/nuxt", "@unocss/nuxt"],
+  modules: ["@pinia/nuxt", "@nuxtjs/i18n", "@vueuse/nuxt", "@unocss/nuxt", "@nuxt/image-edge"],
   i18n: {
     vueI18n: {
       legacy: false,
