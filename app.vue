68d8d0e9821a8e0f5e541e891fc6b6caf836a5d7
hixb <Hello16369@163.com>
2023-08-30 21:07:36 +0800

    style: add plum blossom

--- app.vue
+++ app.vue
@@ -6,7 +6,9 @@
     <main class="pt-8 w-full max-md:pt-24 max-w-[1280px] mx-auto">
       <div class="px-7 relative">
         <div class="flex max-2md:flex-col">
-          <div class="flex-[1_0_calc(100%-var(--my-sidebar-width)-25px)] w-[calc(100%-var(--my-sidebar-width)-25px)] max-2md:w-full">
+          <div
+            class="flex-[1_0_calc(100%-var(--my-sidebar-width)-25px)] w-[calc(100%-var(--my-sidebar-width)-25px)] max-2md:w-full"
+          >
             <NuxtPage />
           </div>
           <BaseAside />
@@ -17,4 +19,5 @@
     <StyleWaves />
   </div>
   <OtherBackTop />
+  <StylePlumBoosom />
 </template>
