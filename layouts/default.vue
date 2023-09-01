b0d0171ec685b73eab8bc56c3f42a9e4ec5ffff0
Xiaobing Zhu <109063364+xiao-ice@users.noreply.github.com>
2023-09-01 11:20:56 +0800

    chore: optimize layouts (#555)

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,23 @@
 </script>
 
 <template>
-  <slot />
+  <div class="flex relative">
+    <BaseSidebar />
+    <main class="pt-8 w-full max-md:pt-24 max-w-[1280px] mx-auto">
+      <div class="px-7 relative">
+        <div class="flex max-2md:flex-col">
+          <div
+            class="flex-[1_0_calc(100%-var(--my-sidebar-width)-25px)] w-[calc(100%-var(--my-sidebar-width)-25px)] max-2md:w-full"
+          >
+            <slot />
+          </div>
+          <BaseAside />
+        </div>
+        <BaseFooter />
+      </div>
+    </main>
+    <StyleWaves />
+  </div>
 </template>
 
 <style scoped lang="scss">
