e10a670853d4408af5ae1fc1bbdbcff0a7f3928a
Xiaobing Zhu <109063364+xiao-ice@users.noreply.github.com>
2023-10-09 11:06:10 +0800

    chore: update readme and code format (#576)
    * style: format
    
    * docs: readme

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,33 +2,33 @@
  * Link association
  */
 type Relation =
-  "alternate"
-  | "author"
-  | "bookmark"
-  | "canonical"
-  | "dns-prefetch"
-  | "external"
-  | "help"
-  | "icon"
-  | "import"
-  | "license"
-  | "manifest"
-  | "modulepreload"
-  | "next"
-  | "nofollow"
-  | "noopener"
-  | "noreferrer"
-  | "opener"
-  | "pingback"
-  | "preconnect"
-  | "prefetch"
-  | "preload"
-  | "prerender"
-  | "prev"
-  | "search"
-  | "shortlink"
-  | "stylesheet"
-  | "tag"
+  'alternate'
+  | 'author'
+  | 'bookmark'
+  | 'canonical'
+  | 'dns-prefetch'
+  | 'external'
+  | 'help'
+  | 'icon'
+  | 'import'
+  | 'license'
+  | 'manifest'
+  | 'modulepreload'
+  | 'next'
+  | 'nofollow'
+  | 'noopener'
+  | 'noreferrer'
+  | 'opener'
+  | 'pingback'
+  | 'preconnect'
+  | 'prefetch'
+  | 'preload'
+  | 'prerender'
+  | 'prev'
+  | 'search'
+  | 'shortlink'
+  | 'stylesheet'
+  | 'tag'
 
 /**
  * Sidebar switching parameters 0-switch 1-open 2-close
