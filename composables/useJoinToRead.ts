e9d670117ba71009e22d3cc546ba45f668dedf61
hixb <Hello16369@163.com>
2024-01-05 21:00:48 +0800

    chore: simplify the log call process

--- composables/useJoinToRead.ts
+++ composables/useJoinToRead.ts
@@ -1,9 +1,10 @@
 export function useJoinToRead() {
   const { get } = useIdbStore()
+  const { log } = useLog()
 
   function setRead<T extends Record<string, any>>(value: T) {
     get(IndexedBDSpace.BDEnum.JOIN_TO_READ)
-      .then(data => useLog('log', data))
+      .then(data => log(data))
   }
 
   return {
--- composables/useLog.ts
+++ composables/useLog.ts
@@ -1,9 +1,22 @@
-export function useLog(mode: 'log' | 'warn' | 'error' | 'info', message: any) {
-  // eslint-disable-next-line n/prefer-global/process
-  if (mode === 'log' && process.env.NODE_ENV !== 'production')
-    // eslint-disable-next-line no-console
-    console[mode](message)
-  else
-    // eslint-disable-next-line no-console
-    console[mode](message)
+export function useLog() {
+  return {
+    log: (message: any) => {
+      // eslint-disable-next-line n/prefer-global/process
+      if (process.env.NODE_ENV !== 'production')
+        // eslint-disable-next-line no-console
+        console.log(message)
+    },
+    warn: (message: any) => {
+      console.warn(message)
+    },
+    error: (message: any) => {
+      console.error(message)
+    },
+    info: (message: any) => {
+      // eslint-disable-next-line n/prefer-global/process
+      if (process.env.NODE_ENV !== 'production')
+      // eslint-disable-next-line no-console
+        console.info(message)
+    },
+  }
 }
